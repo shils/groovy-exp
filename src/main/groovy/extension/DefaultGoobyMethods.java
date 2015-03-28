053ad7fa83300531411eebf0a284fa16abf8dbe4
Shil S <shil.sinha@gmail.com>
2015-03-28 10:46:12 -0400

    trimming

--- src/main/groovy/extension/DefaultGoobyMethods.java
+++ src/main/groovy/extension/DefaultGoobyMethods.java
@@ -13,14 +13,6 @@ import java.util.List;
 
 public class DefaultGoobyMethods {
 
-  public static <T, U extends T, V> V ifInstance(T self, Class<U> clazz, @ClosureParams(SecondParam.FirstGenericType.class) Closure<V> closure) {
-    if (clazz.isInstance(self)) {
-      U cast = (U) self;
-      return closure.call(cast);
-    }
-    return null;
-  }
-
   public static <T> List<T> findAllWith(@DelegatesTo.Target Collection<T> self, @DelegatesTo(genericTypeIndex = 0) Closure<Boolean> closure) {
     List<T> result = new ArrayList<T>();
     for (T t: self) {
@@ -32,15 +24,6 @@ public class DefaultGoobyMethods {
       }
     }
     return result;
-  }
-
-  public static <T> Collection<T> findAll(Iterable<? extends T> self, Collection<T> collector, @ClosureParams(SecondParam.FirstGenericType.class) Closure<Boolean> closure) {
-    for (T t: self) {
-      if (closure.call(t)) {
-        collector.add(t);
-      }
-    }
-    return collector;
   }
 
 }
